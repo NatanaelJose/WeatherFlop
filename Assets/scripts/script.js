85b8a4df0c35eb921e3a8df3e2430634a29cbb12
NatanaelJose <josesaraivamaciel@gmail.com>
2023-06-10 20:52:08 -0300

    add timer and backgrounds

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,20 +1,35 @@
 const cityValue = document.getElementById("city").value;
 const searchBtn = document.getElementById("search");
+const Timer = document.getElementById("Time");
 
 const key = "3ea77c9716c322c3d458735702828971";
+
+const time = setInterval(function() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    if (min < 10) min = '0' + min;
+
+    Timer.innerText = `${hr}:${min}`;
+    if (hr >= 0 && hr < 12) {
+      document.body.style.background = "url('./Assets/backgrounds/flop_morning.png') center / cover no-repeat";
+    } else if (hr >= 12 && hr < 18) {
+      document.body.style.background = "url('./Assets/backgrounds/flop_afternoon.png') center / cover no-repeat";
+    } else {
+      document.body.style.background = "url('./Assets/backgrounds/flop_night.png') center / cover no-repeat";
+    }
+  });
 
 function updateScreen(data) {
     document.getElementById("time-city").innerHTML = "Tempo em " + data.name;
     document.getElementById("temperature").innerHTML = Math.floor(data.main.temp) + " °C";
     document.getElementById("weather-p").innerHTML = data.weather[0].description;
     document.getElementById("umidity").innerHTML = "Umidade: "+ data.main.humidity + "%";
-    if(data.weather[0].icon != "01n") {
-        document.getElementById("img").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-        document.getElementById("img").style.fontSize = none;
 
-    } else {
+    if(data.weather[0].icon === "01n") {
         document.getElementById("img").src = "./Assets/backgrounds/cloud-slash.svg";
-        document.getElementById("img").style.fontSize = 0.7+"em";
+    } else {
+        document.getElementById("img").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
     }
 }
 
